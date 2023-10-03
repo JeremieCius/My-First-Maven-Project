8bd00d92c204b1b4a51144a947adff47dfd78bcd
JeremieCius <jeremiecius@gmail.com>
2023-10-03 15:25:23 -0600

    Open Source 1-1 Complete

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $csc180.cius.jeremie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
