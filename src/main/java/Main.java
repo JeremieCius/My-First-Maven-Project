99132a3e41ef37935a03c4104a01ec58262b874f
JeremieCius <jeremiecius@gmail.com>
2023-10-04 11:36:11 -0600

    Debug 1-4 Complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,31 @@
+import java.io.*;
 public class Main {
-    public static void main (String[] args) {
-        System.out.println("Hello Maven!");
-        System.err.println("DESTROY THE WORLD!!!!!");
+    public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static String testText;
+    public static int loopLimit = 11;
+
+    public static String getTestText() {
+        return testText;
+    }
+
+    public static void setTestText(String testText) {
+        Main.testText = testText;
+    }
+
+    public static int getLoopLimit() {
+        return loopLimit;
+    }
+
+    public static void setLoopLimit(int loopLimit) {
+        Main.loopLimit = loopLimit;
+    }
+
+    public static void main (String[] args) throws IOException {
+        for (int i = 0; i <= loopLimit; i++) {
+            int tries = getLoopLimit() - i;
+            System.out.println("Enter a phrase (You have " + tries + " attempt(s) left)");
+            setTestText(in.readLine());
+            System.err.println(getTestText() + "\n");
+        }
     }
 }
